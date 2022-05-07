642ad6e415e4361f96bdf6b6d269da3e2fa77a31
Alb3rnaru <moreno.alberto907@gmail.com>
2022-05-07 01:37:29 +0000

    :sparkles: Subiendo Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our"];
+  let adjectives = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".us", ".io"];
+  for (let adjective of adjectives) {
+    for (let pronoun of pronouns) {
+      for (let noun of nouns) {
+        for (let extension of extensions) {
+          console.log(pronoun + adjective + noun + extension);
+        }
+      }
+    }
+  }
 };
